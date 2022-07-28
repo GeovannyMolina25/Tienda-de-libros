8add725ecf76ac88bafdad1222b45610b5886207
Nelson molina <geovannysangucho25@gmail.com>
2022-07-27 19:20:19 -0500

    Implementacion de libros

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 
+from fileinput import filename
+import os
 from flask import Flask
 from flask import render_template, request,redirect
 from flaskext.mysql import MySQL
+from datetime import datetime
+from flask import send_from_directory
 
 app=Flask(__name__)
 
@@ -18,6 +22,14 @@ mysql.init_app(app)
 @app.route('/')
 def inicio():
     return render_template('sitio/index.html')
+
+@app.route('/img/<imagen>')
+def imagenes(imagen):
+    print(imagen)
+    return send_from_directory(os.path.join('templates/sitio/img'),imagen)
+
+
+
 
 @app.route('/libros')
 def libros():
@@ -38,8 +50,12 @@ def admin_login():
 @app.route('/admin/libros')
 def admin_libros():
     conexion=mysql.connect()
-    print(conexion)
-    return render_template('admin/libros.html')
+    cursor=conexion.cursor()
+    cursor.execute("SELECT * FROM `libro`" )
+    libros=cursor.fetchall()
+    conexion.commit()
+    print(libros)
+    return render_template('admin/libros.html',libros=libros)
 
 @app.route('/admin/libros/guardar', methods=['POST'])
 def admin_libros_guardar():
@@ -47,8 +63,16 @@ def admin_libros_guardar():
     _url=request.form['txtURL']
     _archivo=request.files['txtImagen']
     
+    tiempo = datetime.now()
+    horaActual=tiempo.strftime('%Y%H%M%S')
+
+    if _archivo.filename!="":
+        nuevoNombre=horaActual+"_"+_archivo.filename
+        _archivo.save("templates/sitio/img/"+nuevoNombre)
+
+
     sql="INSERT INTO `libro` (`id`, `nombre`, `imagen`, `url`) VALUES (NULL, %s,%s,%s);"
-    datos=(_nombre,_archivo.filename,_url)
+    datos=(_nombre,nuevoNombre,_url)
     conexion=mysql.connect()
     cursor=conexion.cursor()
     cursor.execute(sql,datos)
@@ -59,7 +83,23 @@ def admin_libros_guardar():
     print(_archivo)
     return redirect('/admin/libros')
 
+@app.route('/admin/libros/borrar', methods=['POST'])
+def admin_libros_borrar():
+    _id=request.form['txtID']
+    print(_id)
+    conexion=mysql.connect()
+    cursor=conexion.cursor()
+    cursor.execute("SELECT * FROM `libro` WHERE id=%s",(_id) )
+    libro=cursor.fetchall()
+    conexion.commit()
+    print(libro)
 
+    conexion=mysql.connect()
+    cursor=conexion.cursor()
+    cursor.execute("DELETE FROM libro WHERE id=%s",(_id) )
+    conexion.commit()
+    return redirect('/admin/libros')
+    
 
 if __name__ =='__main__':
     app.run(debug=True)
